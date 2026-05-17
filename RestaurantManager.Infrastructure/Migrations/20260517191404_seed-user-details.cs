7f7f0b0dbeca10266c749b3acb7e18b4be40ab5d
NikolayNanov <nanov12@abv.bg>
2026-05-17 22:14:23 +0300

    added seed migration

---
+++ RestaurantManager.Infrastructure/Migrations/20260517191404_seed-user-details.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeduserdetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
