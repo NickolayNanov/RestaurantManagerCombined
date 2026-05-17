d5ba2c95623f31bee003ed86dd2e22805dfbb445
NikolayNanov <nickolaynanov17@gmail.com>
2026-05-17 23:00:32 +0300

    added seed migration

--- RestaurantManager.Application/Handlers/Users/UserInfo/UserInfoResponseHandler.cs
+++ RestaurantManager.Application/Handlers/Users/UserInfo/UserInfoResponseHandler.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RestaurantManager.Application.Exceptions;
 using RestaurantManager.Application.Services.Interfaces;
 using RestaurantManager.Domain;
 
@@ -12,10 +14,15 @@ namespace RestaurantManager.Application.Handlers.Users.UserInfo
     {
         public async Task<UserInfoResponse> Handle(UserInfoResponseQuery request, CancellationToken cancellationToken)
         {
-            var user = await userManager.FindByIdAsync(currentUserService.UserId);
+            var user = await userManager.Users
+                .AsNoTracking()
+                .Include(u => u.ProfileDetails)
+                .FirstOrDefaultAsync(u => u.Id == currentUserService.UserId, cancellationToken)
+                ?? throw new ResourceNotFoundException(nameof(ApplicationUser), $"Could not find user with id: {currentUserService.UserId}");
+
             var roles = await userManager.GetRolesAsync(user);
 
-            return new UserInfoResponse(user.Id, user.UserName, user.Email, roles);
+            return new UserInfoResponse(user.Id, user.UserName, user.Email, roles, user.ProfileDetails?.ProfilePictureUrl);
         }
     }
 }
